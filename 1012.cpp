4c24a11459db9badd6b6162ee21279e1ee36a15d
芝士夜轮 <35161925+NachtgeistW@users.noreply.github.com>
2018-11-01 11:23:46 +0800

    1012

---
+++ 1012.cpp
@@ -0,0 +1,25 @@
+/*
+Problem Description
+I have a very simple problem for you. Given two integers A and B, your job is to calculate the Sum of A + B.
+Input
+The first line of the input contains an integer T(1<=T<=20) which means the number of test cases. Then T lines follow, each line consists of two positive integers, A and B. Notice that the integers are very large, that means you should not process them by using 32-bit integer. You may assume the length of each integer will not exceed 1000.
+Output
+For each test case, you should output two lines. The first line is "Case #:", # means the number of the test case. The second line is the an equation "A + B = Sum", Sum means the result of A + B. Note there are some spaces int the equation. Output a blank line between two test cases.
+Sample Input
+2
+1 2
+112233445566778899 998877665544332211
+Sample Output
+Case 1:
+1 + 2 = 3
+Case 2:
+112233445566778899 + 998877665544332211 = 1111111111111111110
+*/
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    int Case = 0;
+    scanf("%d", Case);
+    return 0;
+}
