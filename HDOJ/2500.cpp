e5f0b55375c31aab181c5fcd8e675357e93fd7c1
芝士夜轮 <35161925+NachtgeistW@users.noreply.github.com>
2018-11-03 18:02:50 +0800

    2500

---
+++ HDOJ/2500.cpp
@@ -0,0 +1,19 @@
+//http://acm.hdu.edu.cn/showproblem.php?pid=2500
+#include<iostream>
+using namespace std;
+int main (void)
+{
+    int N, M;
+    scanf("%d", &N);
+    while(N--)
+    {
+        scanf("%d", &M);
+        for (int i = 1; i <= M * 3; i++)
+        {
+            for (int j = 1; j <= M; j++)
+                printf("HDU");
+        putchar('\n');
+        }
+        
+    }
+}
