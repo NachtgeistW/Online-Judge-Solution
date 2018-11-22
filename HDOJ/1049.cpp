8f481b571228f4d61538ecb68a9ec6ca89857d3c
芝士夜轮 <35161925+NachtgeistW@users.noreply.github.com>
2018-11-22 22:46:22 +0800

    贪心

---
+++ HDOJ/1049.cpp
@@ -0,0 +1,23 @@
+//http://acm.hdu.edu.cn/showproblem.php?pid=1049
+//贪心
+//直接模拟虫子爬的过程
+#include<cstdio>
+int main(void)
+{
+    int n = 0, u = 0, d = 0;
+    while(scanf("%d %d %d", &n, &u, &d) && n != 0)
+    {
+        int time = 0, dist = 0;
+        while(1)
+        {
+            dist += u;
+            time++;
+            if (dist >= n)
+                break;
+            dist -= d;
+            time++;
+        }
+        printf("%d\n", time);
+    }
+    return 0;
+}
